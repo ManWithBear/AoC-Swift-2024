b42b2cc3b02a2129924f8f6127a42116a5c4b2ff
Denis Bogomolov <denis@manwithbear.com>
2024-12-03 01:19:25 +0100

    Solve Day 2 Part 2

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -13,23 +13,34 @@ struct Day02: AdventDay {
 
     // Replace this with your solution for the first part of the day's challenge.
     func part1() -> Any {
-        entities.map {
-            zip($0, $0.dropFirst()).map { $0.0 - $0.1 }
-        }
-        .filter {
-            $0.allSatisfy { $0 > 0 } || $0.allSatisfy { $0 < 0 }
-        }
-        .filter {
-            $0.allSatisfy { (abs($0) <= 3) && (abs($0) >= 1) }
-        }
-        .count
+        entities
+            .filter { isSafe($0) }
+            .count
     }
 
     // Replace this with your solution for the second part of the day's challenge.
     func part2() -> Any {
-        entities.map {
-            $0.count
+        entities.filter {
+            if isSafe($0) {
+                return true
+            }
+            for i in 1...$0.count {
+                let pref = $0.prefix(upTo: i - 1)
+                let suff = $0.dropFirst(i)
+                if isSafe(pref + suff) {
+                    return true
+                }
+            }
+            return false
         }
         .count
+    }
+
+    func isSafe(_ report: some Sequence<Int>) -> Bool {
+        let deltas = zip(report, report.dropFirst()).map { $0.0 - $0.1 }
+        let allNegative = deltas.allSatisfy { $0 < 0 }
+        let allPositive = deltas.allSatisfy { $0 > 0 }
+        let allWithinRange = deltas.allSatisfy { (abs($0) <= 3) && (abs($0) >= 1) }
+        return (allNegative || allPositive) && allWithinRange
     }
 }
